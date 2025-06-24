87fdc2f1e97919fcd11d3145640738d8790be2ef
Mayank Bajaj <mayankbajaj672@gmail.com>
2025-06-24 06:57:48 +0530

    JavaScript conditionals and loops

---
+++ 11_JavaScript/02_Conditionals/script.js
@@ -0,0 +1,39 @@
+// conditionals = if else else-if tenary operator switch case
+// if mei humesha true ya false hi aata hai
+
+// var age = 18;
+
+// if (age > 18) {
+//     console.log("Eligible");
+// }
+// else {
+//     console.log("Not Eligible");
+// }
+
+// else if - jab ek baar or condition check karni ho
+// syntax
+
+// if(condition1){
+//     body
+// }
+// else if (condition2){
+//     body
+// }
+// else{
+//     body
+// }
+
+
+// loops - for  while  do-while  foreach  forin  forof
+
+// for loops syntax
+// for (initialise, condition, increment/decrement) {
+    
+// }
+
+// while
+// initialise
+// while(condition) {
+//     body 
+//     increment/decrement;
+// }
